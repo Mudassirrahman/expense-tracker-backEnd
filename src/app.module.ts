a23b7b708ede161a93678eb14655c0d766af4586
mudassirrahman <mudassirrehman1989@gmail.com>
2023-08-24 20:51:26 +0500

    project moved to initeal level

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { UserEntity } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -14,10 +12,9 @@ import { UserEntity } from './user/entities/user.entity';
       username: 'expensetracker',
       password: 'syedmodelfarm',
       database: 'expensetracker',
-      entities: [UserEntity],
+      entities: [],
       synchronize: true,
     }),
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
